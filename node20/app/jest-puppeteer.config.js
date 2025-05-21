7adeaad58837f2d7f8bc662a654fdf4bfb825b83
Arni <toarni@gmail.com>
2025-05-21 13:10:01 +0200

    refactor: just preparing the next improvements

--- node20/app/jest-puppeteer.config.js
+++ node20/app/jest-puppeteer.config.js
@@ -1,9 +1,15 @@
 const config = {
   launch: {
     acceptInsecureCerts: true,
+    // slowMo: 1000,
     args: [
       '--no-sandbox',
-    ]
+      // '--window-size=1920,1080',
+    ],
+    defaultViewport: {
+      width: 1920,
+      height: 1080
+    },
   }
 };
 
--- node20/app/setup-test-global-utils.js
+++ node20/app/setup-test-global-utils.js
@@ -2,5 +2,9 @@ global.utils = {
   loadPage: async (urlPath) => {
     // page.on('console', msg => console.log('PAGE LOG:', msg.text()));
     return await page.goto(`${global.config.baseUrl}/${urlPath}`, {waitUntil: ['domcontentloaded', 'networkidle0']});
+  },
+  loadPageLoc: async (pageLoc, urlPath) => {
+    // page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+    return await pageLoc.goto(`${global.config.baseUrl}/${urlPath}`, {waitUntil: ['domcontentloaded', 'networkidle0']});
   }
 };
