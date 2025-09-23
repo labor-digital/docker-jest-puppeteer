b76f093b7d98f7f5c171665c0fe0a152197657cb
Arni <toarni@gmail.com>
2025-09-23 12:15:45 +0200

    fix: adding some utils to scroll to the bottom

--- node20/app/setup-test-global-utils.js
+++ node20/app/setup-test-global-utils.js
@@ -1,3 +1,5 @@
+let scrollToBottom = require("scroll-to-bottomjs");
+
 global.utils = {
   loadPage: async (urlPath) => {
     // page.on('console', msg => console.log('PAGE LOG:', msg.text()));
@@ -6,5 +8,29 @@ global.utils = {
   loadPageLoc: async (pageLoc, urlPath) => {
     // page.on('console', msg => console.log('PAGE LOG:', msg.text()));
     return await pageLoc.goto(`${global.config.baseUrl}/${urlPath}`, {waitUntil: ['domcontentloaded', 'networkidle0']});
+  },
+  scrollToBottom: async () => {
+    await page.evaluate(scrollToBottom);
+  },
+  scrollToBottomLoc: async (pageLoc) => {
+    await pageLoc.evaluate(scrollToBottom);
+  },
+  autoScrollBottomLoc: (pageLoc) => {
+    return pageLoc.evaluate(async () => {
+      await new Promise((resolve) => {
+        var totalHeight = 0;
+        var distance = 100;
+        var timer = setInterval(() => {
+          var scrollHeight = document.body.scrollHeight;
+          window.scrollBy(0, distance);
+          totalHeight += distance;
+
+          if(totalHeight >= scrollHeight - window.innerHeight){
+            clearInterval(timer);
+            resolve();
+          }
+        }, 100);
+      });
+    });
   }
 };
